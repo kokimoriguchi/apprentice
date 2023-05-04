6a33ab8b964c29b5119916cb9043c6a475965beb
kokimoriguchi <skoki5231@icloud.com>
2023-05-04 22:23:47 +0900

    パスワードマネージャー更新

--- week3-4/git_practice/git_command.md
+++ week3-4/git_practice/git_command.md
@@ -6,12 +6,11 @@
 - git branch -d <ブランチ名> → ブランチの削除
 
 ## プルリクエスト
-- git branchでmainにいることを確認し、git pull origin mainで最新の状態にする。git statsuで一応確認する。
+- git branchでmainにいることを確認し、git pull origin mainで最新の状態にする。git statusで一応確認する。
 - git checkout -b <新ブランチ名> → -b オプションつけるとブランチを新規作成して切り替える
 - ファイルを変更する。
 - git add <変更したファイル>してgit commit
 - git push origin <新ブランチ名>
 - GitHubに移動しプルリクエストの画面に行きcompareの部分のブランチをさっき作った方にする。そしてcreate pull requestする。レビュワーも選ぶ。
 - コードレビューの人はfile Changedのところで諸々できる。送信するとプルリク送った人に届くのでこのやり取りを続ける。
-(終了したらgit merge origin/mainで統合する。（新しいブランチのまま）でgit add <変更したファイル>してgit commit。一応git statsuする。マージしてるから知らんの増えてるかも。git push origin <さっき作ったブランチ名>。)
 - gitに行くとマージプルリクエストと緑で出ているのでおして、ブランチ削除する。そしたらこの変更分をローカルにも反映させるためにgit checkout mainでブランチ変えて、git pull origin mainで取り込む。git branch -d <さっき作ったブランチ名>いらないから消す。
--- week3-4/linux_work/password.sh
+++ week3-4/linux_work/password.sh
@@ -1,13 +1,70 @@
 #!/bin/bash
-PASSWORD_FILE="passwords.txt"
-echo "パスワードマネージャーへようこそ！"
-echo "サービス名を入力してください："
-read SERVICE_NAME
+# パスワードを保存するファイルのパス
+PASSWORD_FILE="./passwords.txt"
 
-echo "ユーザー名を入力してください："
-read USER_NAME
+# パスワードマネージャーのメニューを表示する関数
+show_menu() {
+  echo "パスワードマネージャーへようこそ！"
+  echo "次の選択肢から入力してください(Add Password/Get Password/Exit):"
+}
 
-echo "パスワードを入力してください："
-read PASSWORD
-echo "${SERVICE_NAME}:${USER_NAME}:${PASSWORD}" >> "${PASSWORD_FILE}"
-echo "Thank you!"
+# パスワードを追加する関数
+add_password() {
+  echo "サービス名を入力してください："
+  read service
+  echo "ユーザー名を入力してください："
+  read username
+  echo "パスワードを入力してください："
+  read password
+
+  # パスワードをファイルに追記する
+  echo "$service,$username,$password" >> $PASSWORD_FILE
+
+  echo "パスワードの追加は成功しました。"
+}
+
+# パスワードを取得する関数
+get_password() {
+  echo "サービス名を入力してください："
+  read service
+
+  # ファイルから該当するサービス名の行を検索する
+  password_line=$(grep "^$service," $PASSWORD_FILE)
+
+  if [ -z "$password_line" ]; then
+    echo "そのサービスは登録されていません。"
+  else
+    # サービス名、ユーザー名、パスワードをカンマ区切りで取得する
+    IFS=',' read -r service_name username password <<< "$password_line"
+    echo "サービス名：$service_name"
+    echo "ユーザー名：$username"
+    echo "パスワード：$password"
+  fi
+}
+
+# パスワードマネージャーのメニューを表示する
+show_menu
+
+# ユーザーの選択に応じて処理を実行する
+while true; do
+  read choice
+
+  case $choice in
+    "Add Password")
+      add_password
+      show_menu
+      ;;
+    "Get Password")
+      get_password
+      show_menu
+      ;;
+    "Exit")
+      echo "Thank you!"
+      exit 0
+      ;;
+    *)
+      echo "入力が間違えています。Add Password/Get Password/Exit から入力してください。"
+      show_menu
+      ;;
+  esac
+done
