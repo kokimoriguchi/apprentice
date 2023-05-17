bd2a9a525de3697910deed3d96a2ca3b1e3b393d
kokimoriguchi <skoki5231@icloud.com>
2023-05-17 13:25:14 +0900

    SQLファイル追加

--- week5-6/quest/internet_tv.sql
+++ week5-6/quest/internet_tv.sql
@@ -1,60 +1,12 @@
-# internetTV
-ここに記載されているSQL文を上から順に打ち込みしていってください。
-環境としてはMySQL５.７以上の実行環境を用意してください。
-
-## 1.ログイン
-MySQLの実行環境準備できましたら下記実施しログインしてください。
-下記コマンドでMySQLサーバーを起動します。
-```
-mysql.server start
-```
-その後下記コマンドでrootユーザーとしてログインしてください。
--pコマンドはパスワードの記述求められるので下記コマンド実施後、パスワードを打ち込みしてください。※パスワード打っても何も表示されないですが打てているので大丈夫です！
-もしパスワード分からなければこちらの記事参考にしてください。[MySQL初期パスワードのありか](https://qiita.com/RyosukeSomeya/items/c695487588f5ac28d9f4)
-また以降の部分知っているので省略したい方はinternet_tv.sqlファイルに全てのコマンド入っていますのでファイルをcloneしていただきmysql -u root -p < internet_tv.sqlでデータ入れ込みをしてから、ログインしてください。
-
-```
-mysql -u root -p
-```
-ログインできましたら一度権限の確認をしてください。
-権限がなければ一部機能使えませんのでご注意ください！
-下記コマンドで現在ログイン中のユーザーの権限確認できます。
-権限付与に関してはこちらの記事参考にしてください。[ユーザーに権限付与するGRANT文](https://www.javadrive.jp/mysql/user/index6.html)
-```
-SHOW GRANTS;
-```
-
-## 2.データベースの作成
-ログインできましたら下記コマンドでデータベースを作成します。
-```
 CREATE DATABASE internet_tv;
-```
-成功した場合は下記での様な表示になります。
-テーブル作成等でも成功した場合は下記のような表示になりますのでご確認ください。
-```
-mysql> CREATE DATABASE internet_tv;
-Query OK, 1 row affected (0.00 sec)
-```
-作成できましたら下記コマンドで使用するデータベースを選択します。
-```
 USE internet_tv
-```
 
-## 3.テーブルの作成
-ここからテーブルの作成に入ります。
-今回は7個のテーブルを作成しますので、下記の各テーブル毎にコマンド実行してください。
-計7回コピペで大丈夫です。
-- channelsテーブルの作成
-```
 CREATE TABLE channels (
   id INT PRIMARY KEY AUTO_INCREMENT,
   channel_number INT NOT NULL UNIQUE,
   channel_name VARCHAR(255) NOT NULL
 );
-```
 
-- productsテーブルの作成
-```
 CREATE TABLE products (
   id INT PRIMARY KEY AUTO_INCREMENT,
   title VARCHAR(255) NOT NULL,
@@ -62,10 +14,7 @@ CREATE TABLE products (
   product_detail VARCHAR(255) NOT NULL,
   release_date DATE NOT NULL
 );
-```
 
-- channel_schedulesテーブルの作成
-```
 CREATE TABLE channel_schedules (
   id INT PRIMARY KEY AUTO_INCREMENT,
   channel_id INT,
@@ -76,10 +25,7 @@ CREATE TABLE channel_schedules (
   FOREIGN KEY (channel_id) REFERENCES channels(id),
   FOREIGN KEY (product_id) REFERENCES products(id)
 );
-```
 
-- seasonsテーブルの作成
-```
 CREATE TABLE seasons (
   id INT PRIMARY KEY AUTO_INCREMENT,
   product_id INT,
@@ -88,9 +34,6 @@ CREATE TABLE seasons (
   FOREIGN KEY (product_id) REFERENCES products(id)
 );
 
-```
-- episodesテーブルの作成
-```
 CREATE TABLE episodes (
   id INT PRIMARY KEY AUTO_INCREMENT,
   season_id INT,
@@ -99,10 +42,8 @@ CREATE TABLE episodes (
   total_episode INT,
   FOREIGN KEY (season_id) REFERENCES seasons(id)
 );
-```
 
-- 番組のジャンルテーブルの作成
-```
+
 CREATE TABLE categories (
   id INT PRIMARY KEY AUTO_INCREMENT,
   product_id INT,
@@ -111,10 +52,7 @@ CREATE TABLE categories (
   FOREIGN KEY (product_id) REFERENCES products(id),
   FOREIGN KEY (episode_id) REFERENCES episodes(id)
 );
-```
 
-- 視聴数テーブルの作成
-```
 CREATE TABLE views (
   id INT PRIMARY KEY AUTO_INCREMENT,
   product_id INT,
@@ -123,12 +61,7 @@ CREATE TABLE views (
   FOREIGN KEY (product_id) REFERENCES products(id),
   FOREIGN KEY (episode_id) REFERENCES episodes(id)
 );
-```
 
-## ４.データの挿入
-
-- channelsテーブルにデータを挿入
-```
 INSERT INTO channels (channel_number, channel_name)
 VALUES
 (1, 'ドラマチャンネル'),
@@ -136,10 +69,7 @@ VALUES
 (3, 'ミステリーチャンネル'),
 (4, 'アクションチャンネル'),
 (5, 'ロマンスチャンネル');
-```
 
-- productsテーブルにデータを挿入
-```
 INSERT INTO products (title, product_minutes, product_detail, release_date)
 VALUES
 ('真夏の夜の夢', 60, '夏の夜のロマンスドラマ', '2023-05-01'),
@@ -154,10 +84,7 @@ VALUES
 ('冬の謎', 60, '冬のミステリードラマ', '2023-05-10'),
 ('プログラミング入門', 45, '初心者向けプログラミング講座', '2023-05-11'),
 ('デザインマスター', 60, 'デザインの基礎から応用まで', '2023-05-12');
-```
 
-- channel_schedulesテーブルにデータを挿入
-```
 INSERT INTO channel_schedules (channel_id, product_id, start_time_schedule, end_time_schedule, broadcast_date)
 VALUES
 (1, 1, '20:00:00', '21:00:00', '2023-05-01'),
@@ -171,10 +98,6 @@ VALUES
 (4, 9, '20:00:00', '21:00:00', '2023-05-14'),
 (5, 10, '20:00:00', '21:00:00', '2023-05-15');
 
-```
-
-- seasonsテーブルにデータを挿入
-```
 INSERT INTO seasons (product_id, season_number, total_season)
 VALUES
 (1, 1, 1),
@@ -188,10 +111,6 @@ VALUES
 (9, 1, 1),
 (10, 1, 1);
 
-```
-
-- episodesテーブルにデータを挿入
-```
 INSERT INTO episodes (season_id, episode_title, episode_number, total_episode)
 VALUES
 (1, '夢の始まり', 1, 10),
@@ -205,10 +124,6 @@ VALUES
 (9, '自己成長の秘訣', 1, 10),
 (10, 'ヘルシーレシピ', 1, 10);
 
-```
-
-- categoriesテーブルにデータを挿入
-```
 INSERT INTO categories (product_id, episode_id, category_name)
 VALUES
 (1, 1, 'ロマンス'),
@@ -221,10 +136,7 @@ VALUES
 (8, 8, 'ビジネス'),
 (9, 9, '自己啓発'),
 (10, 10, '健康');
-```
 
-- viewsテーブルにデータを挿入
-```
 INSERT INTO views (product_id, episode_id, view_count)
 VALUES
 (1, 1, 10000),
@@ -237,4 +149,4 @@ VALUES
 (8, 8, 22000),
 (9, 9, 19000),
 (10, 10, 21000);
-```
+
