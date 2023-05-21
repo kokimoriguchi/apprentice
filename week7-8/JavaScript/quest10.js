75192973b79026f4f73369a27b18aba18c4d348c
kokimoriguchi <skoki5231@icloud.com>
2023-05-21 18:27:15 +0900

    Jsquest10まで実施

---
+++ week7-8/JavaScript/quest10.js
@@ -0,0 +1,27 @@
+const hasOdd = (numbers) => {
+  for (let i = 0; i < numbers.length; i++ ){
+    if (numbers[i] % 2 !== 0){
+      console.log(true)
+    }
+  }
+  console.log(false)
+};
+hasOdd([1,2,3,4,5]);
+
+const odd = (numbers) => {
+  for (let i = 0; i < numbers.length; i++ ){
+    if (numbers[i] % 2 !== 0){
+      console.log(numbers[i])
+    }
+  }
+}
+odd([1,2,3,4,5])
+
+const square = (numbers) => {
+  const result = [];
+  for (let i = 0; i < numbers.length; i++){
+    return result = numbers[i]**
+    console.log(result);
+  }
+}
+square([1,2,3,4,5])
---
+++ week7-8/JavaScript/quest7.js
@@ -0,0 +1,7 @@
+const num_1 = 5;
+const num_2 = 3;
+
+let sum = num_1 + num_2;
+let difference = num_1 - num_2;
+
+console.log(sum + difference);
