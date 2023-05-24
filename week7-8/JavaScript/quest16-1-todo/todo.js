cbdba72f2b918b901bcbc150245dd5b26dcf5a0a
kokimoriguchi <skoki5231@icloud.com>
2023-05-24 13:50:43 +0900

    todoアプリ完成リファクタリング必須

--- week7-8/JavaScript/quest16-1-todo/todo.js
+++ week7-8/JavaScript/quest16-1-todo/todo.js
@@ -2,37 +2,63 @@
 // 各タスクには削除ボタンが付いており、削除ボタンを押すとタスクが削除される
 // 各タスクにはチェックボックスが付いており、チェックボックスにチェックを入れるとタスクが完了したことになり、タスクの文字に取り消し線が付く。チェックを外すと取り消し線が消える
 
-const addButton = document.getElementById('add-button')
-const newTodo = document.getElementById('todo-list')
-
-//デリートボタン追加する関数
-const addDeleteButton = () => {
-  const deleteButton =document.createElement('button')
-  deleteButton.classList.add('delete-button')
-  deleteButton.textContent = "削除"
-  newTodo.appendChild(deleteButton);
-};
-
-const deleteTodo = (event) => {
-  const parent = event.target.parentNode;
-  parent.removeChild(addList);
-  parent.removeChild(deleteButton);
-}
-
+//
 const addTodo = (event) => {
   //送信の効果を無効化
   event.preventDefault();
+
+  //ul要素の中にdivタグ作成しその中に新しいtodoとデリートボタン、チェックボックス入れていく。
+  const addDiv = document.createElement('div')
+  addDiv.classList.add('todo-content')
+  newTodo.appendChild(addDiv)
 
   //todoの追加する関数。中でデリートボタン呼び出す関数もよんでいる
   const todoInput = document.getElementById('todo-input').value
   const addList = document.createElement('li')
   addList.classList.add('todo-item');
   addList.textContent = todoInput
-  newTodo.appendChild(addList);
+  addDiv.appendChild(addList);
+  document.getElementById('todo-input').value = ""
 
-  addDeleteButton()
+  //ボタン追加する関数
+  const addButton = () => {
+
+    //デリートボタン追加する関数
+    const deleteButton = document.createElement('button')
+    deleteButton.classList.add('delete-button')
+    deleteButton.textContent = "削除"
+    addDiv.appendChild(deleteButton);
+
+    //チェックボックス追加する関数
+    const checkBox = document.createElement('input');
+    checkBox.setAttribute('type', 'checkbox');
+    addDiv.appendChild(checkBox);
+
+    //デリートボタン押した時にクリックイベント発火で削除する関数
+    const deleteTodo = (event) => {
+      const parent = event.target.parentNode;
+      parent.removeChild(addList);
+      parent.removeChild(deleteButton);
+      parent.removeChild(checkBox)
+    }
+    deleteButton.addEventListener('click', deleteTodo )
+
+    //チェックボックスクリックで打ち消し線出す関数
+    const checkEvent = (event) => {
+      const checkbox = event.target;
+      const listItem = checkbox.parentNode;
+
+      if (event.target.checked) {
+        listItem.style.textDecoration = 'line-through';
+      } else {
+        listItem.style.textDecoration = 'none';
+      }
+    };
+    checkBox.addEventListener('click', checkEvent)
+  };
+  addButton()
 };
 
+const addButton = document.getElementById('add-button')
+const newTodo = document.getElementById('todo-list')
 addButton.addEventListener('click', addTodo);
-deleteButton.addEventListener('click', deleteTodo )
-
