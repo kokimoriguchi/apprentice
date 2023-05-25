a229f90a3730c9d3e76d9238854eba5361480c77
kokimoriguchi <skoki5231@icloud.com>
2023-05-25 18:14:49 +0900

    ESLint

--- week7-8/JavaScript/quest18.js
+++ week7-8/JavaScript/quest18.js
@@ -1,12 +1,12 @@
 function checkDivisibleByFive(num) {
   // 処理を記述
-  const answer = num % 5 === 0;
-  try{
-    if(num % 5 !== 0){
-      throw new Error('数値は5で割り切れません');
+  try {
+    if (num % 5 !== 0) {
+      throw new Error("数値は5で割り切れません");
     }
     console.log(true);
-  } catch (event){
+  } catch (event) {
     console.log(event.message);
   }
 }
+checkDivisibleByFive(7);
